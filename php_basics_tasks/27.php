8862ac0b7b5fbd96aea826696f1dca7abcef2581
AshMorrow <koragg666@gmail.com>
2016-02-09 20:01:54 +0200

    homework part2

---
+++ php_basics_tasks/27.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>console</title>
+</head>
+<body>
+    <h3>Консоль с utf8 не дружит :(</h3>
+    <img src="http://i75.fastpic.ru/big/2016/0209/6f/a7dd36c898b49196aafa12902f968e6f.png" alt="console">
+</body>
+</html>
