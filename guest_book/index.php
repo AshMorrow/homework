d9a70c0ce60975a7506a811b8cd8d4ed188b7dda
AshMorrow <koragg666@gmail.com>
2016-02-22 02:32:53 +0200

    guest book alpha

--- guest_book/index.php
+++ guest_book/index.php
@@ -14,24 +14,38 @@ define('DS',DIRECTORY_SEPARATOR);
         <script src="./js/bootstrap.min.js" type="text/javascript"></script>
 </head>
 <body>
-<div class="container">
-    <div class="col-md-6">
-    <form action="<?=$_SERVER['REQUEST_URI']?>" method="post" class="form-group">
-        <p>Введите свое имя</p>
-        <input type="userName" type="text" name="userName" class="form-control">
-        <p>Оставте свой комент</p>
-        <textarea name="userMassage" cols="36" rows="10" class="form-control"></textarea><br>
-        <input type="submit" name="submit" class="btn btn-success">
-    </form>
-    </div>
-    <div class="col-md-6">
-<?php
-    include ROOT.DS."libs.php";
-    $message = getContent();
-    $message =addComment($message);
-    showComment($message);
-?>
+<div class="page-wrap">
+    <div class="container">
+        <header>
+            <div class="pull-right">
+                <a href="./login.php" class="loginBtn">Войти</a>
+            </div>
+            <div class="clearfix"></div>
+        </header>
+        <div class="col-md-12" style="float: none; margin: 0px auto;">
+            <h1 class="headH1">Гостевая книга</h1>
+            <?php
+                include ROOT.DS."libs.php";
+                $message = getContent();
+                $message = addComment($message);
+                showComment($message);
+            ?>
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-md-12" style="float: none; margin: 0px auto;">
+        <h2 class="headH2">Добывить сообщение</h2>
+        <form action="<?=$_SERVER['REQUEST_URI']?>" method="post" class="form-group">
+            <input type="userName" type="text" name="userName" placeholder="Введите свое имя" class="form-control"
+                   required>
+            <br>
+            <textarea name="userMassage" cols="36" rows="10" placeholder="Оставте свой комент" class="form-control" required></textarea><br>
+            <input type="submit" name="submit" class="addBtn button">
+        </form>
+        </div>
     </div>
 </div>
+<footer>
+
+</footer>
 </body>
 </html>
--- guest_book/libs.php
+++ guest_book/libs.php
@@ -1,7 +1,7 @@
 <?php
 
-function deleteMassage(){
-    if($_POST){
+function deleteMassage(){/* Функция удаления коментария */
+    if(isset($_POST['remove'])){
         $massage = array_reverse(getContent());
         $id = $_POST['remove'];
         unset($massage[$id]);
@@ -9,19 +9,23 @@ function deleteMassage(){
         file_put_contents("1massage.db", $massageDB);
     }
 }
-function cenZor($post){
-    /*(file_put_contents(ROOT.DS.'cencored'.DS.'cen.db',str_ireplace(',',PHP_EOL,file_get_contents(ROOT.DS.'cencored'.DS.'cen.db')));
-    $arr = explode(PHP_EOL,file_get_contents(ROOT.DS.'cencored'.DS.'cen.db'));
-    foreach($arr as $k => $v){
-        $arr[$k] = trim($v);
-    }*/
-    $cens = explode(PHP_EOL,file_get_contents(ROOT.DS.'cencored'.DS.'cen.db'));
-    foreach($cens as $word){
-        $post["userName"] = str_ireplace($word,"МАТ",$post['userName']);
-        $post["userMassage"] = str_ireplace($word,"МАТ",$post["userMassage"]);
+
+function cenZor($post,$key){/* Функция цензурирования */
+    $cens = getBadWord();
+    $postTemp["userName"] = md5($post["userName"]);
+    $postTemp["userMassage"] = md5($post["userMassage"]);
+    foreach ($cens as $word) {
+        $post["userName"] = str_ireplace($word, "МАТ", $post['userName']);
+        $post["userMassage"] = str_ireplace($word, "МАТ", $post['userMassage']);
+    }
+    if($postTemp["userName"] != md5($post["userName"])){
+        $post["cencored"] = $key;
+    }elseif( $postTemp["userMassage"] != md5($post["userMassage"])){
+        $post["cencored"] = $key;
     }
     return $post;
 }
+
 function getContent(){
     if(is_readable("1massage.db")){
         $massage = file_get_contents("1massage.db");
@@ -32,7 +36,6 @@ function getContent(){
 }
 function addComment($massage){
     if(isset($_POST['submit'])) {
-        //echo var_dump($_POST);
         $newPost['userName'] = htmlspecialchars($_POST['userName']);
         $newPost['userMassage'] = htmlspecialchars($_POST['userMassage']);
         $massage[] = $newPost;
@@ -41,23 +44,81 @@ function addComment($massage){
     }
     return $massage;
 }
+
 function showComment($massage,$adm = 0){
-    if(isset($massage)){
+    if (isset($massage)) {
         $massage = array_reverse($massage);
         foreach ($massage as $k => $post) {
-            $post = cenZor($post);
-            ?>
-                <div class=""><sub>Пользователь</sub> <strong><?= $post['userName'] ?></strong> <?php if($adm){ ?>
-                        <form action="" method="post">
-                            <button type="submit" value="<?= $k; ?>" name="remove" class="btn-danger btn">Удалить
-                            </button>
-                        </form>
-                    <?php } ?></div>
-                <div class="panel-body">
-                    <p><?=nl2br($post['userMassage']) ?></p>
-                </div>
-            <?php
+            //if(!$adm){
+
+            //}
+            $post = cenZor($post,$k);
+            showCommentHtml($post,$adm,$k);
         }
     }
 }
+
+function showCommentHtml($post,$adm,$k){ /**HTML часть вывода коментариев****/
+    ?>
+    <div class="<?php
+    if($adm == 0){ echo 'commentBlock '; }else{ echo 'adminCommentBlock ';}
+    if(isset($post['cencored'])){ echo "cencored";}else {echo "notCencored";} ?>">
+        <div class="userNameBlock"><span class="users">Пользователь</span> <strong
+                class="userName"><?= $post['userName']
+                ?></strong>
+            <?php if ($adm) { ?>
+                <form action="" method="post" class="delForm">
+                    <button type="submit" value="<?=$k; ?>" name="remove" class="delFormBtn">Удалить
+                    </button>
+                </form>
+            <?php } ?></div>
+        <div class="panel-body">
+            <p><?= nl2br($post['userMassage']) ?></p>
+        </div>
+    </div>
+    <?php
+}
+
+function getBadWord(){
+    if(is_readable(ROOT . DS . 'cencored' . DS . 'cen.db')){
+        $cen = explode(PHP_EOL, file_get_contents(ROOT . DS . 'cencored' . DS . 'cen.db'));
+    }
+    return $cen;
+}
+function addBadWord($cen){
+    if(isset($_POST['submit'])) {
+        $newBadWord = explode(PHP_EOL,strip_tags($_POST['badWord']));
+        $newBadWord = array_unique($newBadWord);
+        foreach($newBadWord as $val) {
+            file_put_contents(ROOT . DS . 'cencored' . DS . 'cen.db', $val.PHP_EOL,FILE_APPEND);
+        }
+        return $newBadWord;
+    }
+}
+function showBadWods($cen){
+    print_r($cen);
+}
+function userLogin(){
+    if(isset($_POST["submit"])){
+        $users = json_decode(file_get_contents("users/passwd"),true);
+        var_dump($users);
+        if($users["login"] == md5($_POST["userLogin"]) && $users["password"] == md5($_POST["userPass"])){
+            if ($_SESSION) {
+                $_SESSION["sessionId"] = session_id();
+                header("Location: ./adm.php");
+                exit;
+            }
+
+        }
+    }
+}
+function userLogout(){
+    if(isset($_POST["exit"])){
+        unset($_SESSION["sessionId"]);
+        header("Location: ./login.php");
+    }elseif(!isset($_SESSION["sessionId"])){
+        header("Location: ./login.php");
+    }
+}
 ?>
+
